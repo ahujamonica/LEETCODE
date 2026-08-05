92bea467ba2344c27a9d44b9c27cd265321983a1
Monica Ahuja <ahujamonica9321@gmail.com>
2026-08-06 01:50:40 +0530

    Update Solution.java

--- 01 - Arrays/0073-Set-Matrix-Zeroes/Solution.java
+++ 01 - Arrays/0073-Set-Matrix-Zeroes/Solution.java
@@ -1,1 +1,131 @@
+Approach 1: Brute Force
 
+  class Solution {
+
+    private void markRow(int[][] matrix, int row) {
+        for (int j = 0; j < matrix[0].length; j++) {
+            if (matrix[row][j] != 0)
+                matrix[row][j] = -1;
+        }
+    }
+
+    private void markCol(int[][] matrix, int col) {
+        for (int i = 0; i < matrix.length; i++) {
+            if (matrix[i][col] != 0)
+                matrix[i][col] = -1;
+        }
+    }
+
+    public void setZeroes(int[][] matrix) {
+
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+
+                if (matrix[i][j] == 0) {
+                    markRow(matrix, i);
+                    markCol(matrix, j);
+                }
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+
+                if (matrix[i][j] == -1)
+                    matrix[i][j] = 0;
+            }
+        }
+    }
+}
+
+Approach 2: Better (Using Extra Space)
+
+  class Solution {
+    public void setZeroes(int[][] matrix) {
+
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        int[] row = new int[m];
+        int[] col = new int[n];
+
+        // Mark rows and columns
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+
+                if (matrix[i][j] == 0) {
+                    row[i] = 1;
+                    col[j] = 1;
+                }
+            }
+        }
+
+        // Update matrix
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+
+                if (row[i] == 1 || col[j] == 1) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+}
+
+
+Approach 3: Optimized (Constant Extra Space)
+
+  class Solution {
+
+    public void setZeroes(int[][] matrix) {
+
+        boolean firstRow = false;
+        boolean firstCol = false;
+
+        // Step 1: Store markers
+        for (int i = 0; i < matrix.length; i++) {
+
+            for (int j = 0; j < matrix[0].length; j++) {
+
+                if (matrix[i][j] == 0) {
+
+                    // Preserve original first row / first column
+                    if (i == 0) firstRow = true;
+                    if (j == 0) firstCol = true;
+
+                    // Use first row & first column as markers
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+
+        // Step 2: Update inner matrix
+        for (int i = 1; i < matrix.length; i++) {
+
+            for (int j = 1; j < matrix[0].length; j++) {
+
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        // Step 3: Update first row
+        if (firstRow) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+
+        // Step 4: Update first column
+        if (firstCol) {
+            for (int i = 0; i < matrix.length; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+}
