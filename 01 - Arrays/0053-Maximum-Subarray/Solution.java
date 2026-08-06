3b531f7111a6f833a772c1be68896b60745bb2ae
Monica Ahuja <ahujamonica9321@gmail.com>
2026-08-06 08:26:34 +0530

    Optimize maxSubArray using Kadane's Algorithm
    Refactor maxSubArray method to implement Kadane's Algorithm for O(N) time complexity.

--- 01 - Arrays/0053-Maximum-Subarray/Solution.java
+++ 01 - Arrays/0053-Maximum-Subarray/Solution.java
@@ -26,3 +26,52 @@ Result: ❌ Time Limit Exceeded (200 / 210 test cases passed)
 Although this brute-force approach correctly generates every possible subarray and computes its sum, its O(N³) time complexity makes it impractical for large inputs. The repeated recalculation of overlapping subarray sums causes the solution to exceed the time limit.
 
  
+# Approach 2 - Better Solution :
+
+    class Solution {
+    public int maxSubArray(int[] nums) {
+        int max = Integer.MIN_VALUE;
+        int n = nums.length;
+
+        for(int i = 0; i < n; i++){
+            int sum = 0;
+            for(int j = i; j < n; j++){
+                sum = sum + nums[j];
+                max = Math.max(max, sum);
+            }
+        }
+        return max;
+    }
+}
+
+Result: ❌ Time Limit Exceeded — 205 / 210 testcases passed
+
+Notice the improvement though:
+
+O(N³) → 200 / 210
+O(N²) → 205 / 210
+
+So your optimization worked, but we need to go one step further: O(N).
+
+# Approach 3 - Kadane's Algorithm
+
+    class Solution {
+    public int maxSubArray(int[] nums) {
+        int sum = 0;
+        int max = Integer.MIN_VALUE;
+
+        for(int i = 0; i < nums.length; i++){
+            sum = sum + nums[i];
+
+            if(sum > max){
+                max = sum;
+            }
+
+            if(sum < 0){
+                sum = 0;
+            }
+        }
+
+        return max;
+    }
+}
