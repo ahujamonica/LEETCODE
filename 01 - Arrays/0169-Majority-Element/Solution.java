afe0379407599a88fb7491568d244fb22b36cfa7
Monica Ahuja <ahujamonica9321@gmail.com>
2026-08-09 15:02:39 +0530

    Implement majority element algorithm

--- 01 - Arrays/0169-Majority-Element/Solution.java
+++ 01 - Arrays/0169-Majority-Element/Solution.java
@@ -1,1 +1,30 @@
+class Solution {
+    public int majorityElement(int[] nums) {
 
+        int candidate = 0;
+        int count = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            // If there are no active votes,
+            // choose the current element as the candidate
+            if (count == 0) {
+                candidate = nums[i];
+            }
+
+            // Same element supports the candidate
+            if (nums[i] == candidate) {
+                count++;
+            }
+
+            // Different element cancels one vote
+            // of the current candidate
+            else {
+                count--;
+            }
+        }
+
+        // The problem guarantees that a majority element exists
+        return candidate;
+    }
+}
