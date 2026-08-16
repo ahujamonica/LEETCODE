afff74e0cfcbb56f168e44c2734ea2619d47d2b3
Monica Ahuja <ahujamonica9321@gmail.com>
2026-08-17 00:28:17 +0530

    Implement reverseList method to reverse a linked list

--- 02 - LL/0206. Reverse Linked List/Solution.java
+++ 02 - LL/0206. Reverse Linked List/Solution.java
@@ -1,1 +1,43 @@
 
+class Solution {
+    public ListNode reverseList(ListNode head) {
+
+        // If the list is empty, there is nothing to reverse
+        if (head == null) {
+            return null;
+        }
+
+        // If the list contains only one node,
+        // it is already reversed
+        if (head.next == null) {
+            return head;
+        }
+
+        // Initially, there is no previous node
+        ListNode preNode = null;
+
+        // Start from the head of the original list
+        ListNode currentNode = head;
+
+        // Process every node
+        while (currentNode != null) {
+
+            // Save the next node before changing the pointer
+            ListNode nextNode = currentNode.next;
+
+            // Reverse the current node's pointer
+            currentNode.next = preNode;
+
+            // Move preNode to the current node
+            preNode = currentNode;
+
+            // Move currentNode to the saved next node
+            currentNode = nextNode;
+        }
+
+        // preNode is now the new head of the reversed list
+        head = preNode;
+
+        return head;
+    }
+}
