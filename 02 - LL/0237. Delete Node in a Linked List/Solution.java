35d8872e6b2db2c8b938430eb258c8fbd2123275
Monica Ahuja <ahujamonica9321@gmail.com>
2026-08-20 00:28:14 +0530

    Implement deleteNode method to remove a node

--- 02 - LL/0237. Delete Node in a Linked List/Solution.java
+++ 02 - LL/0237. Delete Node in a Linked List/Solution.java
@@ -1,1 +1,10 @@
+class Solution {
+    public void deleteNode(ListNode node) {
 
+        // Copy the next node's value
+        node.val = node.next.val;
+
+        // Skip the next node
+        node.next = node.next.next;
+    }
+}
