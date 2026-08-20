43876642c128b929396bf5e5365aacb9962726f8
Monica Ahuja <ahujamonica9321@gmail.com>
2026-08-20 19:46:33 +0530

    Implement intersection detection for two linked lists

--- 02 - LL/0160. Intersection of Two Linked Lists/Solution.java
+++ 02 - LL/0160. Intersection of Two Linked Lists/Solution.java
@@ -1,1 +1,41 @@
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
 
+        // Find the length of both lists
+        int lenA = getListLength(headA);
+        int lenB = getListLength(headB);
+
+        // Move the longer list forward
+        while (lenA > lenB) {
+            lenA--;
+            headA = headA.next;
+        }
+
+        while (lenB > lenA) {
+            lenB--;
+            headB = headB.next;
+        }
+
+        // Move both pointers together
+        // until they point to the same node
+        while (headA != headB) {
+            headA = headA.next;
+            headB = headB.next;
+        }
+
+        // Intersection node or null
+        return headA;
+    }
+
+    private int getListLength(ListNode head) {
+
+        int len = 0;
+
+        while (head != null) {
+            len++;
+            head = head.next;
+        }
+
+        return len;
+    }
+}
