cc750255a3fa761ced04a1a6cfc80e052eade9ed
Monica Ahuja <ahujamonica9321@gmail.com>
2026-08-24 17:56:03 +0530

    Implement cycle detection in linked list

--- 02 - LL/0142. Linked List Cycle II/Solution.java
+++ 02 - LL/0142. Linked List Cycle II/Solution.java
@@ -1,1 +1,29 @@
 
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        // Detect the cycle
+        while (fast != null && fast.next != null) {
+
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+
+                // Find the start of the cycle
+                while (head != slow) {
+                    head = head.next;
+                    slow = slow.next;
+                }
+
+                return slow;
+            }
+        }
+
+        // No cycle
+        return null;
+    }
+}
