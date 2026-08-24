24a79fa6ae40840f96c0fb2374649d9a79874a2b
Monica Ahuja <ahujamonica9321@gmail.com>
2026-08-24 16:32:49 +0530

    Add reverseKGroup method to reverse nodes in k-groups
    Implemented reverseKGroup method to reverse nodes in k-sized groups.

--- 02 - LL/0025. Reverse Nodes in k-Group/Solution.java
+++ 02 - LL/0025. Reverse Nodes in k-Group/Solution.java
@@ -1,1 +1,34 @@
 
+class Solution {
+    public ListNode reverseKGroup(ListNode head, int k) {
+
+        // Check if at least k nodes are available
+        ListNode curr = head;
+
+        for (int i = 0; i < k; i++) {
+            if (curr == null) {
+                return head;
+            }
+
+            curr = curr.next;
+        }
+
+        // Reverse exactly k nodes
+        ListNode prev = null;
+        curr = head;
+
+        for (int i = 0; i < k; i++) {
+            ListNode next = curr.next;
+
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        // Recursively process the remaining groups
+        head.next = reverseKGroup(curr, k);
+
+        // prev is the new head of the reversed group
+        return prev;
+    }
+}
