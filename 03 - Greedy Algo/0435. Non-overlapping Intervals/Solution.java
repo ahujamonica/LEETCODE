c532aadeefcf45240bb983d63efe26bb23e603f0
Monica Ahuja <ahujamonica9321@gmail.com>
2026-08-27 19:34:27 +0530

    Add solution for erasing overlapping intervals
    Implemented a solution to erase overlapping intervals by sorting and counting non-overlapping ones.

--- 03 - Greedy Algo/0435. Non-overlapping Intervals/Solution.java
+++ 03 - Greedy Algo/0435. Non-overlapping Intervals/Solution.java
@@ -1,1 +1,27 @@
 
+class Solution {
+    public int eraseOverlapIntervals(int[][] intervals) {
+
+        if (intervals.length == 0) {
+            return 0;
+        }
+
+        // Sort intervals by end time
+        Arrays.sort(intervals, Comparator.comparingInt(a -> a[1]));
+
+        int count = 1;
+        int previous_interval = 0;
+
+        for (int i = 1; i < intervals.length; i++) {
+
+            // Current start >= previous end
+            if (intervals[i][0] >= intervals[previous_interval][1]) {
+                previous_interval = i;
+                count++;
+            }
+        }
+
+        // Total intervals - intervals kept
+        return intervals.length - count;
+    }
+}
